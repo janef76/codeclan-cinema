e52e6ab496785d7b2a0f76fe06dd1005b25b2e38
janef76 <janeflucker@btinternet.com>
2018-02-16 16:17:27 +0000

    inner joins added

--- models/customer.rb
+++ models/customer.rb
@@ -1,4 +1,5 @@
 require_relative('../db/sql_runner.rb')
+require_relative('film')
 
 class Customer
   attr_reader :id
@@ -48,5 +49,16 @@ class Customer
     SqlRunner.run(sql, values)
   end
 
+  def films()
+    sql = "SELECT films.*
+    FROM films
+    INNER JOIN tickets
+    ON films.id = tickets.film_id
+    WHERE tickets.customer_id = $1"
+    values = [@id]
+    films = SqlRunner.run(sql, values)
+    result = films.map {|film|Film.new(film)}
+    return result
+  end
 
 end
--- models/film.rb
+++ models/film.rb
@@ -48,4 +48,16 @@ class Film
       SqlRunner.run(sql, values)
     end
 
+    def customers()
+      sql = "SELECT customers.*
+      FROM customers
+      INNER JOIN tickets
+      ON customers.id = tickets.customer_id
+      WHERE tickets.film_id = $1"
+      values = [@id]
+      customers = SqlRunner.run(sql, values)
+      result = customers.map {|customer|Customer.new(customer)}
+      return result
+    end
+
 end
