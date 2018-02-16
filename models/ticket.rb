531146225839c8caf26c09d5bed03d31008255d8
janef76 <janeflucker@btinternet.com>
2018-02-16 15:11:45 +0000

    MVP completed

--- models/ticket.rb
+++ models/ticket.rb
@@ -28,4 +28,39 @@ class Ticket
       @id = ticket['id'].to_i
   end
 
+  def update()
+    sql = "UPDATE tickets SET
+    (
+      customer_id,
+      film_id
+    )
+    =
+    ($1, $2
+    )
+    WHERE id = $3"
+    values = [@customer_id, @film_id, @id]
+    SqlRunner.run(sql, values)
+  end
+
+  def delete()
+    sql = "DELETE FROM tickets
+    WHERE id = $1"
+    values = [@id]
+    SqlRunner.run(sql, values)
+  end
+
+  def customer()
+      sql = "SELECT * FROM customers WHERE id = $1"
+      values = [@customer_id]
+      customer = SqlRunner.run(sql, values).first
+      return Customer.new(customer)
+  end
+
+  def film()
+    sql = "SELECT * FROM films WHERE id = $1"
+    values = [@film_id]
+    film = SqlRunner.run(sql, values).first
+    return Film.new(film)
+  end
+
 end
